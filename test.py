791c1d70d9c8208d1800ce9aceb3baaaf3d9c60e
mrxiuer <2413543249@qq.com>
2022-07-03 22:55:23 +0800

    2022/7/3

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-def test(int):
-    int = int*2
-    print(int)
-get_num = int(input("请输入你的数字:"))
-test(get_num)
+def test(string):
+    print("调用前：",string)
+    string +=string
+    print("调用后：",string)
+str1 = ['123','jds']
+str2 = ('232,','323')
+str3 = "323wsw"
+test(str1)
+test(str2)
+test(str3)
+# 为什么字符串最终也增加了？
