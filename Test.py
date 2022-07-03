ffdcde3f90f8d949129c5f6b5180d680acf0a354
mrxiuer <2413543249@qq.com>
2022-07-03 16:04:34 +0800

    Changes

--- Test.py
+++ Test.py
@@ -3,4 +3,9 @@ import re
 mail = input("请输入你的邮箱地址：")
 temp = r'@|\.'
 spil = re.split(temp,mail)
-print("收件人的用户名:",spil[0],end=" "+"邮件服务器名:"+spil[1])
+temp1 = r'\d{10}'
+num = re.match(temp1,spil[0],re.I)
+if num != None:
+    print("收件人的用户名:",spil[0],end=" "+"邮件服务器名:"+spil[1])
+else:
+    print("输入错误!")
