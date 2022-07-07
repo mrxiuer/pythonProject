512a1363f3e96c3dec0fa7d2df8ba404b50ad2e3
mrxiuer <2413543249@qq.com>
2022-07-07 23:00:28 +0800

    2022/7/4

--- ccf/Test.py
+++ ccf/Test.py
@@ -1,14 +1,6 @@
-import sys
-import time
-
-def bar(num, total):
-    rate = num / total
-    rate_num = int(rate * 100)
-    r = '\r[%s%s]%d%%' % ("="*num, " "*(100-num), rate_num, )
-    sys.stdout.write(r)
-    sys.stdout.flush()
-
-
-for i in range(0, 101):
-    time.sleep(0.1)
-    bar(i, 100)
+import math
+r=10
+result = lambda r:math.pi*r*r
+print(result(r))
+r=20
+print(result(r))
