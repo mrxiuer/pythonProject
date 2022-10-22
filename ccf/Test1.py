edf03bed7516858bce7b274d5fd18f0d574ee854
mrxiuer <2413543249@qq.com>
2022-10-22 20:50:12 +0800

    Changes

--- ccf/Test1.py
+++ ccf/Test1.py
@@ -1,24 +1,28 @@
 from socket import *
 from time import ctime
+
 HOST = ''
 PORT = 21567
 BUFSIZ = 1024
-ADDR = (HOST,PORT)
+ADDR = (HOST, PORT)
 
-tcpSerSock = socket(AF_INET,SOCK_STREAM)
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
 
 while True:
-    print("waiting for connection...")
-    tcpClisock,addr = tcpSerSock.accept()
-    print('...connected from:',addr)
+    print("waiting for connection>>>")
+    tcpClisock, addr = tcpSerSock.accept()
+    print("connected success!")
 
     while True:
         data = tcpClisock.recv(BUFSIZ).decode(encoding='utf-8')
         if not data:
             break
-        data = '[%s] %s'%(ctime(),data)
+        data = "[%s] %s" % (ctime(), data)
         print(data)
-        tcpClisock.send(data.encode(encoding='utf-8'))
+        data = input("请输入你要发送的讯息:").encode(encoding='utf-8')
+        print("waiting for message>>>")
+        tcpClisock.send(data)
+
     tcpClisock.close()
--- ccf/Test1_ex.py
+++ ccf/Test1_ex.py
@@ -1,4 +1,6 @@
 from socket import *
+from time import ctime
+
 HOST = 'localhost'
 PORT = 21567
 BUFSIZ = 1024
@@ -6,14 +8,14 @@ ADDR = (HOST,PORT)
 
 tcpClisock = socket(AF_INET,SOCK_STREAM)
 tcpClisock.connect(ADDR)
-
 while True:
-    data = input('> ').encode(encoding='utf-8')
+    data = input("请输入讯息：").encode(encoding="utf-8")
     if not data:
         break
     tcpClisock.send(data)
+    print("waiting for message...")
     data = tcpClisock.recv(BUFSIZ).decode(encoding='utf-8')
     if not data:
         break
-    print(data)
-tcpClisock.close()
+    print("message has got!")
+    print("[%s] %s"%(ctime(),data))
