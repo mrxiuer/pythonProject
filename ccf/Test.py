de6509833bc041abb8751e601b6982dc19cd3b5d
mrxiuer <2413543249@qq.com>
2023-03-15 16:01:39 +0800

    2023/3/15

--- ccf/Test.py
+++ ccf/Test.py
@@ -1,7 +1,4 @@
-import bmi1 as b
-num1 = float(input("请输入第一个数："))
-num2 = float(input("请输入第二个数："))
-b.add(num1,num2)
-b.dele(num1,num2)
-b.squ(num1,num2)
-b.chu(num1,num2)
+f=open("message.txt",'r+')
+f.write('123\n')
+for line in f.readlines():
+    print(line)
